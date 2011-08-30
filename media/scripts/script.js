2f3a5df3718891eac22e9e3edb2fa1cc259e6351
Josh Nielsen <Josh@joshontheweb.com>
2011-08-29 23:15:45 -0700

    fixed broken links

--- media/scripts/script.js
+++ media/scripts/script.js
@@ -1,17 +1,17 @@
 (function($) {
     
-    window.app.start();
-    
-
-    $('.main-nav').click(function(e){
-        e.preventDefault();
-        var $anchor = $(e.target)
-        var href = $anchor.attr('href');
-        $('body').animate({scrollTop: $(href).offset().top - 50});
-    });
-    
-    
-    $('#jquery-test').text('jQuery is loaded')
+    // window.app.start();
+      //   
+      // 
+      //   $('.main-nav').click(function(e){
+      //       e.preventDefault();
+      //       var $anchor = $(e.target)
+      //       var href = $anchor.attr('href');
+      //       $('body').animate({scrollTop: $(href).offset().top - 50});
+      //   });
+      //   
+      //   
+      //   $('#jquery-test').text('jQuery is loaded')
 })(jQuery)
 
 
